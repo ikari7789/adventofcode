9ac63f0cb8177f38ee676af33c6360447f5bf322
Andrew Miller <ikari7789@yahoo.com>
2020-12-22 22:03:37 +0900

    wip: 2020-12-12

---
+++ 2020/12/12.php
@@ -0,0 +1,17 @@
+<?php
+
+$inputFile = '12_test.txt';
+
+/**
+ */
+
+$input = new SplFileObject($inputFile);
+
+while (! $input->eof()) {
+    $line = $input->fgets();
+    $line = trim($line);
+
+    if (empty($line)) {
+        continue;
+    }
+}
