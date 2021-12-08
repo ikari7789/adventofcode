b241ad834a8a2b0d407375d3ae11ba11811813fa
Andrew Miller <ikari7789@yahoo.com>
2021-12-08 13:58:11 +0900

    2021-12-07

---
+++ 2021/07/app.php
@@ -0,0 +1,67 @@
+<?php
+
+define('DEBUG', true);
+define('INPUT_FILE', 'example_input.txt');
+
+$input = new SplFileObject(INPUT_FILE);
+
+$crabs = explode(',', trim($input->fgets()));
+
+$distances = array_count_values($crabs);
+$closest   = min($crabs);
+$furthest  = max($crabs);
+
+// Part 1
+$totals = [];
+for ($waypoint = $closest; $waypoint <= $furthest; ++$waypoint) {
+    $totals[$waypoint] = 0;
+
+    foreach ($distances as $distance => $count) {
+        if ($waypoint < $distance) {
+            $totals[$waypoint] += ($distance - $waypoint) * $count;
+        }
+
+        if ($distance < $waypoint) {
+            $totals[$waypoint] += ($waypoint - $distance) * $count;
+        }
+    }
+}
+
+$leastGases = array_keys($totals, min($totals));
+
+if (count($leastGases) > 1) {
+    echo 'More than one answer!' . PHP_EOL;
+}
+
+foreach ($leastGases as $leastGas) {
+    printf('Way point %d costs %d' . PHP_EOL, $leastGas, $totals[$leastGas]);
+}
+
+// Part 2
+$totals = [];
+for ($waypoint = $closest; $waypoint <= $furthest; ++$waypoint) {
+    $totals[$waypoint] = 0;
+
+    foreach ($distances as $distance => $count) {
+        if ($waypoint < $distance) {
+            $cost = array_sum(range(1, ($distance - $waypoint)));
+        }
+
+        if ($distance < $waypoint) {
+            $cost = array_sum(range(1, ($waypoint - $distance)));
+        }
+
+        $totals[$waypoint] += $cost * $count;
+    }
+}
+
+$leastGases = array_keys($totals, min($totals));
+
+if (count($leastGases) > 1) {
+    echo 'More than one answer!' . PHP_EOL;
+}
+
+foreach ($leastGases as $leastGas) {
+    printf('Way point %d costs %d' . PHP_EOL, $leastGas, $totals[$leastGas]);
+}
+
