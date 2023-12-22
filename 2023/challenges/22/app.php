87a335feb879566ec8e5b3ecf2d4f34eea255164
Andrew Miller <ikari7789@yahoo.com>
2023-12-21 23:10:30 -0600

    2023-12-22 wip

---
+++ 2023/challenges/22/app.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+define('DEBUG', true);
+define('INPUT_FILE', __DIR__ . '/example_input.txt');
+
+$input = new SplFileObject(INPUT_FILE);
+while (! $input->eof() && $line = $input->fgets()) {
+    $line = trim($line);
+}
